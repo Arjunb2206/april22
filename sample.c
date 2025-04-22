e1867697ae183e2c2de495d78d87e330e2e76641
arjunb2206 <arjun.b@s.amity.edu>
2025-04-22 09:49:32 +0530

    edit in sample.c

--- sample.c
+++ sample.c
@@ -5,7 +5,7 @@ int main()
 	int sum,a,b,c;//int variables 
 	printf("enter three  number");
 	scanf("%d,%d,%d",&a,&b,&c);//input elements
-	sum=a+b+c;
-	printf("sum is : %d",sum);//sum of number
+	sum=a+b+c;//sum of numbers 
+	printf("sum is : %d",sum);//sum 
 	return 0;
 }
