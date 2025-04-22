7f0422c7a3c5f9d0e050a0ed47d3b606db69e6a5
arjunb2206 <arjun.b@s.amity.edu>
2025-04-22 09:51:10 +0530

    comment in sample.c

--- sample.c
+++ sample.c
@@ -5,8 +5,8 @@ int main()
 	int sum,sub,a,b;
 	printf("enter a number");
 	scanf("%d %d",&a,&b);//input two number
-	sum=a+b;
-	sub=a-b;
+	sum=a+b;//sum
+	sub=a-b;//sub
 	printf("sum is : %d",sum);//sum of number
 	printf("sub  is :%d",sub);//sub of number
 	return 0;
