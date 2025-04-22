49a0ffe9023ac464653b1ac7bc6f9183326179ab
arjunb2206 <arjun.b@s.amity.edu>
2025-04-22 09:30:46 +0530

    comment to three number |sample.c

--- sample.c
+++ sample.c
@@ -2,10 +2,10 @@
 #include<stdio.h>
 int main()
 {
-	int sum,a,b,c;
+	int sum,a,b,c;//int variables 
 	printf("enter three  number");
-	scanf("%d,%d,%d",&a,&b,&c);
+	scanf("%d,%d,%d",&a,&b,&c);//input elements
 	sum=a+b+c;
-	printf("sum is : %d",sum);
+	printf("sum is : %d",sum);//sum of number
 	return 0;
 }
