1bfe764df5addd301e676b596ef6b3cd21b475bd
adithya20-afk <adithya.kp@s.amity.edu>
2025-05-13 10:42:02 +0530

    sum of three to four numbers|sample.c

--- sample.c
+++ sample.c
@@ -1,12 +1,12 @@
-//sum of three number c program is written :
+//sum of four number c program is written :
 #include<stdio.h>
 int main()
 {
-	int sum,a,b,c;//int variables 
+	int sum,a,b,c,d;//int variables 
 	printf("enter three  number");
-	scanf("%d,%d,%d",&a,&b,&c);//input elements
-	sum=a+b+c;//sum of numbers 
-	sub=a+b+c;//sub of numbers
+	scanf("%d,%d,%d,%d",&a,&b,&c,&d);//input elements
+	sum=a+b+c+d;//sum of numbers 
+	sub=a+b+c+d;//sub of numbers
 	printf("sum is : %d",sum);//sum of number
 	printf("sub  is :%d",sub);//sub of number
 	return 0;
